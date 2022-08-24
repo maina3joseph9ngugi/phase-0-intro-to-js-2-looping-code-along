1e5f805123968bf1efed2376b823d414ddcfde86
maina3joseph9ngugi <110392941+maina3joseph9ngugi@users.noreply.github.com>
2022-08-24 07:51:36 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames ,event){
+let thankYouArray=[]
+for(let i=0; i<arrayOfNames.length;i++){
+    let messages =`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`;
+    thankYouArray.push(messages)
+}
+return thankYouArray
+}
+function countDown(initialNumber){
+    while(initialNumber>0){
+        console.log(initialNumber);
+initialNumber=initialNumber-1
+    }
+    console.log (initialNumber)
+}
+
+
